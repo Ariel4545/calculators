a6b467d6e1aacdb9e6a53cf83125d34ca8419b61
Ariel <95249974+Ariel4545@users.noreply.github.com>
2022-09-17 19:23:51 +0300

    🎉 added size settings

--- EgonCalc.py
+++ EgonCalc.py
@@ -1,14 +1,15 @@
 # imports
+import tkinter.messagebox
 from tkinter import *
 
 # window
 root = Tk()
 width = 340
 height = 430
-screen_width = root.winfo_width()
-screen_height = root.winfo_height()
-placement_x = abs((screen_width // 2) - (width // 2))
-placement_y = abs((screen_height // 2) - (height // 2))
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+placement_x = round((screen_width // 2) - (width // 2))
+placement_y = round((screen_height // 2) - (height // 2))
 root.geometry(f'{width}x{height}+{placement_x}+{placement_y}')
 root.title("Egon calculator")
 root.resizable(False, False)
@@ -79,6 +80,55 @@ def button_div():
     entry.delete(0, END)
 
 
+def settings(event=None):
+    # window
+    settings_root = Toplevel()
+    settings_root.resizable()
+    # text and buttons
+    settings_text = Label(settings_root, text='Settings', font='arial 14 bold')
+    size_text = Label(settings_root, text='Size settings:',font=('arial 10 underline'), pady=5)
+    size_small = Button(settings_root, text='Small')
+    size_normal = Button(settings_root, text='Medium')
+    size_big = Button(settings_root, text='Big')
+    settings_text.grid(row=0)
+    size_text.grid(row=1)
+    size_small.grid(row=2, column=0)
+    size_normal.grid(row=2, column=1)
+    size_big.grid(row=2, column=2)
+
+    def size():
+        global padx_b,  pady_b
+        for i in b_list:
+            i.config(padx=padx_b, pady=pady_b)
+        root.geometry(f'{width}x{height}')
+
+    def small():
+        global padx_b,  pady_b
+        global width, height
+        padx_b, pady_b = 1, 3
+        size_small.config(bg='grey'), size_normal.config(bg='white'), size_big.config(bg='white')
+        width, height = 340, 430
+        size()
+
+    def medium():
+        global padx_b,  pady_b
+        global width, height
+        padx_b, pady_b = 2, 6
+        width, height = 355, 455
+        size_small.config(bg='white'), size_normal.config(bg='grey'), size_big.config(bg='white')
+        size()
+
+    def big():
+        global padx_b,  pady_b
+        global width, height
+        padx_b, pady_b = 4, 12
+        width, height = 370, 500
+        size_small.config(bg='white'), size_normal.config(bg='white'), size_big.config(bg='grey')
+        size()
+
+    size_small.config(command=small), size_normal.config(command=medium), size_big.config(command=big)
+    small()
+
 
 button_frame = Frame(root, padx=20)
 button_frame.grid(row=1)
@@ -87,26 +137,38 @@ padx_b = 1
 pady_b = 3
 button_height = 6
 button_width = 10
-b1 = Button(button_frame, text="1", command=lambda: button_click(1), padx=padx_b, pady=pady_b, relief=FLAT, height=button_height
+b1 = Button(button_frame, text="1", command=lambda: button_click(1), padx=padx_b, pady=pady_b, relief=FLAT,
+            height=button_height
             , width=button_width)
-b2 = Button(button_frame, text="2", command=lambda: button_click(2), padx=padx_b, pady=pady_b, relief=FLAT, height=button_height
+b2 = Button(button_frame, text="2", command=lambda: button_click(2), padx=padx_b, pady=pady_b, relief=FLAT,
+            height=button_height
             , width=button_width)
-b3 = Button(button_frame, text="3", command=lambda: button_click(3), padx=padx_b, pady=pady_b, relief=FLAT, height=button_height
+b3 = Button(button_frame, text="3", command=lambda: button_click(3), padx=padx_b, pady=pady_b, relief=FLAT,
+            height=button_height
             , width=button_width)
-b4 = Button(button_frame, text="4", command=lambda: button_click(4), padx=padx_b, pady=pady_b, relief=FLAT, height=button_height
+b4 = Button(button_frame, text="4", command=lambda: button_click(4), padx=padx_b, pady=pady_b, relief=FLAT,
+            height=button_height
             , width=button_width)
-b5 = Button(button_frame, text="5", command=lambda: button_click(5), padx=padx_b, pady=pady_b, relief=FLAT, height=button_height
+b5 = Button(button_frame, text="5", command=lambda: button_click(5), padx=padx_b, pady=pady_b, relief=FLAT,
+            height=button_height
             , width=button_width)
-b6 = Button(button_frame, text="6", command=lambda: button_click(6), padx=padx_b, pady=pady_b, relief=FLAT, height=button_height
+b6 = Button(button_frame, text="6", command=lambda: button_click(6), padx=padx_b, pady=pady_b, relief=FLAT,
+            height=button_height
             , width=button_width)
-b7 = Button(button_frame, text="7", command=lambda: button_click(7), padx=padx_b, pady=pady_b, relief=FLAT, height=button_height
+b7 = Button(button_frame, text="7", command=lambda: button_click(7), padx=padx_b, pady=pady_b, relief=FLAT,
+            height=button_height
             , width=button_width)
-b8 = Button(button_frame, text="8", command=lambda: button_click(8), padx=padx_b, pady=pady_b, relief=FLAT, height=button_height
+b8 = Button(button_frame, text="8", command=lambda: button_click(8), padx=padx_b, pady=pady_b, relief=FLAT,
+            height=button_height
             , width=button_width)
-b9 = Button(button_frame, text="9", command=lambda: button_click(9), padx=padx_b, pady=pady_b, relief=FLAT, height=button_height
+b9 = Button(button_frame, text="9", command=lambda: button_click(9), padx=padx_b, pady=pady_b, relief=FLAT,
+            height=button_height
             , width=button_width)
-b0 = Button(button_frame, text="0", command=lambda: button_click(0), padx=padx_b, pady=pady_b, relief=FLAT, height=button_height
+b0 = Button(button_frame, text="0", command=lambda: button_click(0), padx=padx_b, pady=pady_b, relief=FLAT,
+            height=button_height
             , width=button_width)
+
+b_list = [b1, b2, b3, b4, b4, b5, b6, b7, b8, b9]
 
 # placing numerical buttons
 b1.grid(row=1, column=0)
@@ -144,7 +206,7 @@ div_b = Button(button_frame, text="/", padx=padx_oper, pady=pady_oper, command=l
 clear_b = Button(button_frame, text="X", padx=padx_oper, pady=pady_oper, command=lambda: button_clear(), relief=FLAT,
                  height=button_height, bg=operation_color
                  , width=button_width)
-
+b_list += equal_b, add_b, sub_b, mul_b, div_b, clear_b
 # placing operations buttons
 equal_b.grid(row=4, column=1)
 add_b.grid(row=1, column=4)
@@ -162,5 +224,9 @@ root.bind('<c>', button_clear)
 root.bind('<e>', lambda event: button_equal())
 # root.bind('<1>', lambda event: button_click(1))
 root.bind('<Escape>', lambda event: root.quit())
+root.bind('<s>', settings)
 
+tkinter.messagebox.showinfo('Tip', 'for the settings to pop up press s')
 root.mainloop()
+
+# bug fixes + settings (size)
